937f625a4dede4534b84a753ec2da92514188e3c
Deepankar Raizada <draizada@smartsignin.com>
2013-07-24 16:56:53 +0530

    Versioning

---
+++ Items/Version.cs
@@ -0,0 +1,39 @@
+﻿#region Copyright (c) & ChangeLog
+/*===============================================================================
+' Copyright (c) 2013 Studio Netsouls Pvt. Ltd. All rights reserved.
+' All rights reserved.
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+' OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+' LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+' FITNESS FOR A PARTICULAR PURPOSE.
+'
+' Change Log
+'
+' AUTHOR:   Deepankar Raizada
+'===============================================================================*/
+#endregion
+
+using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("Tranzfreight Logistics India Pvt. Ltd.")]
+[assembly: AssemblyCopyright("Copyright © Tranzfreight Logistics India Pvt. Ltd. 2012")]
+[assembly: AssemblyTrademark("Studio Netsouls Pvt. Ltd.")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("Debug")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below: 
+// Versioning logic yyyy.mm.dd
+[assembly: AssemblyVersion("2013.07.09")]
+[assembly: AssemblyFileVersion("2013.07.09")]
+[assembly: AssemblyInformationalVersion("2013.07.09")]
