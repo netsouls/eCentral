1b85594e6e990a8d4320f8f1dfa7023f923a3024
Deepankar Raizada <draizada@smartsignin.com>
2013-08-24 18:02:55 +0530

    Reset Path Build Task

---
+++ Tools/eCentral.Tools.CssPaths.BuildTask/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("eCentral.Tools.CssPaths.BuildTask")]
+[assembly: AssemblyDescription("eCentral Css Paths Build Task")]
+[assembly: AssemblyProduct("eCentral.Tools.CssPaths.BuildTask")]
+[assembly: AssemblyCompany("Tranzfreight Logistics India Pvt. Ltd.")]
+[assembly: AssemblyCopyright("Copyright © Tranzfreight Logistics India Pvt. Ltd. 2013")]
+[assembly: AssemblyTrademark("Studio Netsouls Pvt. Ltd.")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("Release")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("da460af7-5781-4990-b2d5-a35c8be1e965")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2013.08.24")]
+[assembly: AssemblyFileVersion("2013.08.24")]
---
+++ Tools/eCentral.Tools.CssPaths.BuildTask/ResetPathTask.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace eCentral.Tools.CssPaths.BuildTask
+{
+    /// <summary>
+    /// MSBuild task to reset all the path
+    /// </summary>
+    public class ResetPathTask : Task
+    {
+        [Required]
+        public ITaskItem[] SourceFiles { get; set; }
+
+        public override bool Execute()
+        {
+            var relativePath = "/library/images";
+
+            // we need to read each individual file and reset the paths in that file
+            foreach (var sourceFile in SourceFiles)
+            {
+                var cssContent = File.ReadAllText(sourceFile.ItemSpec);
+                // replace image paths
+                var relativePaths = FindDistinctRelativePathsIn(cssContent);
+
+                foreach ( var data in relativePaths)
+                {
+                    cssContent = ReplaceRelativePathsIn(cssContent, data, relativePath);
+                }
+
+                // save the file again
+                File.WriteAllText(sourceFile.ItemSpec, cssContent);
+            }
+
+            return true;
+        }
+
+        private string ReplaceRelativePathsIn(string css, string oldPath, string newPath)
+        {
+            var regex = new Regex(@"url\([""']{0,1}" + Regex.Escape(oldPath) + @"[""']{0,1}\)", RegexOptions.IgnoreCase);
+
+            return regex.Replace(css, delegate(Match match)
+            {
+                var path = match.Value.Replace(oldPath, string.Format("{0}/{1}", newPath, oldPath));
+                return path;
+            });
+        }
+
+        private IEnumerable<string> FindDistinctRelativePathsIn(string css)
+        {
+            var matches = Regex.Matches(css, @"url\([""']{0,1}(.+?)[""']{0,1}\)", RegexOptions.IgnoreCase);
+            var matchesHash = new HashSet<string>();
+            foreach (Match match in matches)
+            {
+                var path = match.Groups[1].Captures[0].Value;
+                if (!path.StartsWith("/") && !path.StartsWith("http://") && !path.StartsWith("https://") && !path.StartsWith("data:") && !path.StartsWith("squishit://"))
+                {
+                    if (matchesHash.Add(path))
+                    {
+                        yield return path;
+                    }
+                }
+            }
+        }
+    }
+}
